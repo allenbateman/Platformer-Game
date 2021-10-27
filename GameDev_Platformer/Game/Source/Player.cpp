0a564bd85f1f5d29649d4922fba5018ec1de6c78
ivjocab <72105672+ivjocab@users.noreply.github.com>
2021-10-27 22:52:43 +0200

    Player movement improved + Implementing player animations

--- GameDev_Platformer/Game/Source/Player.cpp
+++ GameDev_Platformer/Game/Source/Player.cpp
@@ -2,11 +2,13 @@
 #include "App.h"
 #include "Input.h"
 #include "ModulePhysics.h"
+#include "Render.h"
 #include "Player.h"
 #include <SDL/include/SDL_scancode.h>
 
 ModulePlayer::ModulePlayer() : Module()
 {
+	name.Create("player");
 }
 
 ModulePlayer::~ModulePlayer()
@@ -20,15 +22,33 @@ bool ModulePlayer::Start()
 
 	//Initializing player struct data
 	p = new Player1;
+	pState = IDLE;
 	//p->player = app->physics->CreateRectangle(20, 300, 20, 40, b2_dynamicBody);
 	p->player = app->physics->CreateCircle(20, 300, 20,b2_dynamicBody);
 	p->player->body->SetFixedRotation(true);
 	//Idle anim
-	p->idlePlayerAnim.PushBack({ 0, 0, 39, 38 });
+	p->idlePlayerAnim.PushBack({ 262, 11, 16, 21 });
+	p->idlePlayerAnim.PushBack({ 294, 11, 16, 21 });
+	p->idlePlayerAnim.PushBack({ 327, 11, 16, 21 });
+	p->idlePlayerAnim.loop = false;
+	p->idlePlayerAnim.mustFlip = false;
+	p->idlePlayerAnim.speed = 0.02f;
 	//Walking anim
-	p->idlePlayerAnim.PushBack({ 0, 0, 39, 38 });
+	p->walkingPlayerAnim.PushBack({ 390, 11, 16, 21 });
+	p->walkingPlayerAnim.PushBack({ 454, 11, 16, 21 });
+	p->walkingPlayerAnim.loop = true;
+	p->walkingPlayerAnim.mustFlip = false;
+	p->walkingPlayerAnim.speed = 0.06f;
 	//Death anim
-	p->idlePlayerAnim.PushBack({ 0, 0, 39, 38 });
+	p->jumpingPlayerAnim.PushBack({ 390, 11, 16, 21 });
+	p->jumpingPlayerAnim.PushBack({ 422, 11, 16, 21 });
+	p->jumpingPlayerAnim.PushBack({ 454, 11, 16, 21 });
+	p->jumpingPlayerAnim.PushBack({ 486, 11, 16, 21 });
+	p->jumpingPlayerAnim.loop = true;
+	p->jumpingPlayerAnim.speed = 0.06f;
+
+	playerTexture= app->tex->Load("Assets/Spritesx16/characters.png");
+
 
 	return true;
 }
@@ -54,6 +74,24 @@ bool ModulePlayer::SaveState(pugi::xml_node&) const
 // Update: draw background
 bool ModulePlayer::Update(float dt)
 {
+	switch (pState)
+	{
+	case IDLE:
+		currentAnim = &p->idlePlayerAnim;
+		break;
+
+	case WALK:
+		currentAnim = &p->walkingPlayerAnim;
+		break;
+
+	case JUMP:
+		currentAnim = &p->jumpingPlayerAnim;
+		break;
+	}
+
+	app->render->DrawTexture(playerTexture, METERS_TO_PIXELS(p->player->body->GetPosition().x - 9), 
+				 METERS_TO_PIXELS(p->player->body->GetPosition().y), &(currentAnim->GetCurrentFrame()), 1.0f);
+
 	bool ret = true;
 	//Player movement
 	maxSpeedX = 0.6;
@@ -62,22 +100,42 @@ bool ModulePlayer::Update(float dt)
 	if ((app->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT) && (p->player->body->GetLinearVelocity().x <= maxSpeedX))
 	{
 		p->player->body->SetLinearVelocity({ 0.6, p->player->body->GetLinearVelocity().y });
+		pState = WALK;
+		p->walkingPlayerAnim.Update();
+		p->idlePlayerAnim.Reset();
+		p->IsDirectionRight = true;
 	}
 	//Left
 	if ((app->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT) && (p->player->body->GetLinearVelocity().x >= minSpeedX))
 	{
 		p->player->body->SetLinearVelocity({ -0.6, p->player->body->GetLinearVelocity().y });
+		pState = WALK;
+		p->walkingPlayerAnim.Update();
+		p->idlePlayerAnim.Reset();
+		p->IsDirectionRight = false;
 	}
 	//Jump
 	if ((app->input->GetKey(SDL_SCANCODE_UP) == KEY_DOWN) && (p->player->body->GetLinearVelocity().y == 0))
 	{
-		p->player->body->ApplyLinearImpulse({ 0, -1.1f }, { 0, 0 }, true);
-	}
+		p->player->body->ApplyLinearImpulse({ 0, -1.6f }, { 0, 0 }, true);
+		pState = JUMP;
+		p->jumpingPlayerAnim.Update();
+		p->idlePlayerAnim.Reset();
+		p->walkingPlayerAnim.Reset();
+	}	
 	////DoubleJump
-	//if ((app->input->GetKey(SDL_SCANCODE_UP) == KEY_DOWN) && (p->player->body->GetLinearVelocity().y > 0))
+	//if ((app->input->GetKey(SDL_SCANCODE_UP) == KEY_DOWN) && (p->player->body->GetLinearVelocity().y < 0))
 	//{
 	//	p->player->body->ApplyLinearImpulse({ 0, -1.1f }, { 0, 0 }, true);
 	//}
+	if (p->player->body->GetLinearVelocity().x == 0 && p->player->body->GetLinearVelocity().y == 0)
+	{
+		pState = IDLE;
+		p->idlePlayerAnim.Update();
+		p->walkingPlayerAnim.Reset();
+	}
+
+
 
 	return ret;
 }
--- GameDev_Platformer/Game/Source/Player.h
+++ GameDev_Platformer/Game/Source/Player.h
@@ -3,17 +3,18 @@
 #include "Globals.h"
 #include "Point.h"
 #include "Animation.h"
+#include "Textures.h"
 
 struct Player1 {
 	PhysBody* player;
 	Animation idlePlayerAnim;
-	Animation deathPlayerAnim;
+	Animation jumpingPlayerAnim;
 	Animation walkingPlayerAnim;
 	enum State
 	{
 		IDLE,
-		DEAD,
-		SPAWN
+		WALK,
+		JUMP
 	};
 	bool IsDirectionRight;
 };
@@ -33,8 +34,20 @@ public:
 	bool LoadState(pugi::xml_node&);
 	bool SaveState(pugi::xml_node&) const;
 
+	SDL_Texture* playerTexture;
+
 public:
 	Player1* p;
+	Animation* currentAnim = nullptr;
+	Animation idlePlayerAnim, walkingPlayerAnim, jumpingPlayerAnim;
+	enum State
+	{
+		IDLE,
+		WALK,
+		JUMP
+	};
+	State pState;
+	bool IsDirectionRight;
 	float maxSpeedX;
 	float minSpeedX;
 };
