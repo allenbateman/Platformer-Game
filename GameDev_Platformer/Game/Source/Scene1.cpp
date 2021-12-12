b8da7f5ec78dcc396afa459e6ffbe5996e8b04c1
ivjocab <72105672+ivjocab@users.noreply.github.com>
2021-12-12 02:08:32 +0100

    Revert "Music stops after finishing game"
    This reverts commit 6eef818b8c5b71471848200611cc83134aa2eed5.

--- GameDev_Platformer/Game/Source/Scene1.cpp
+++ GameDev_Platformer/Game/Source/Scene1.cpp
@@ -53,7 +53,8 @@ bool Scene1::Start()
 
 	props = app->tex->Load("Assets/Spritesx16/props.png");
 	music = app->audio->LoadFx("Assets/audio/music/level1.wav");
-	app->audio->PlayMusic("Assets/audio/music/level1.wav", 2.0f);
+	
+	app->audio->PlayFx(music);
 
 	//Portal animations
 	//Idle anim
@@ -266,7 +267,6 @@ bool Scene1::CleanUp()
 	app->bat->Disable();
 	app->map->CleanUp();
 	app->physics->Disable();
-	app->audio->PlayMusic("Assets/Audio/lol.wav", 0);
 	
 	return true;
 }
@@ -283,7 +283,6 @@ void Scene1::Disable()
 	app->player->Disable();
 	app->map->CleanUp();
 	app->physics->Disable();
-	app->audio->Disable();
 }
 
 bool Scene1::LoadState(pugi::xml_node& data)
